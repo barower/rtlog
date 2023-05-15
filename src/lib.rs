278db05c5f34a1ad62528a6f71ef047938193ec8
Bartłomiej Osiński <osinskibartlomiej@gmail.com>
2023-05-15 23:55:24 +0200

    Basic log numbering

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,38 @@
-use std::sync::mpsc;
+use std::sync::{
+    Arc,
+    mpsc,
+    atomic::AtomicU64
+};
 
 pub struct Rtl;
-pub struct RtlSender(mpsc::Sender<String>);
-pub struct RtlReceiver(mpsc::Receiver<String>);
+
+pub struct RtlSender{
+    sender: mpsc::Sender<String>,
+    counter: Arc<AtomicU64>
+}
+
+pub struct RtlReceiver{
+    receiver: mpsc::Receiver<String>,
+}
 
 impl Rtl {
     pub fn channel() -> (RtlSender, RtlReceiver) {
+        let counter = Arc::new(AtomicU64::new(0));
         let (sender, receiver) = mpsc::channel();
-        (RtlSender(sender), RtlReceiver(receiver))
+        (RtlSender{sender, counter}, RtlReceiver{receiver})
     }
 }
 
 impl RtlSender {
     pub fn send(&self, log: String) {
-        self.0.send(log).unwrap();
+        let counter = self.counter.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        let log = format!("{}: {}", counter, log);
+        self.sender.send(log).unwrap();
     }
 }
 
 impl RtlReceiver {
     pub fn recv(&self) -> String {
-        self.0.recv().unwrap()
+        self.receiver.recv().unwrap()
     }
 }
